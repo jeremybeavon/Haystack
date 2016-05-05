cb5fc079a6b66f5fcb7190de2995b9402abc85f3
jeremybeavon <jeremybeavon@users.noreply.github.com>
2016-05-06 06:40:51 +1200

    Add PostSharp ammender

---
+++ Haystack.Diagnostics.Amendments.PostSharp.Net45/HaystackEntryAspect.cs
@@ -0,0 +1,50 @@
+﻿using Haystack.Bootstrap;
+using Haystack.Diagnostics.ObjectModel;
+using PostSharp.Aspects;
+using System;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace Haystack.Diagnostics.Amendments.PostSharp
+{
+    [Serializable]
+    public sealed class HaystackEntryAspect : OnMethodBoundaryAspect
+    {
+        public HaystackEntryAspect()
+        {
+            AspectPriority = int.MinValue;
+        }
+
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            HaystackBootstrapInitializer.InitializeIfNecessary();
+            BeforeMethodCall(args);
+        }
+
+        private void BeforeMethodCall(MethodExecutionArgs args)
+        {
+            MethodCallTraceProvider provider = MethodCallTraceContext.MethodCallTrace;
+            MethodBase method = args.Method;
+            MethodInfo methodInfo = method as MethodInfo;
+            MethodCall methodCall = new MethodCall()
+            {
+                DeclaringTypeIndex = provider.GetTypeIndex(method.DeclaringType),
+                InstanceIndex = provider.GetObjectIndex(args.Instance),
+                MethodName = method.Name,
+                Parameters = provider.GetParameters(args.Arguments, method.GetParameters()),
+            };
+            if (methodInfo != null)
+            {
+                methodCall.ReturnTypeIndex = provider.GetTypeIndex(methodInfo.ReturnType);
+            }
+
+            if (method.Attributes.HasFlag(MethodAttributes.SpecialName) && Regex.IsMatch(method.Name, "^[gs]et_"))
+            {
+                methodCall.PropertyType = method.Name.StartsWith("get_") ? PropertyType.Get : PropertyType.Set;
+                methodCall.MethodName = methodCall.MethodName.Substring(4);
+            }
+
+            provider.EnterMethodCall(methodCall);
+        }
+    }
+}
---
+++ Haystack.Diagnostics.Amendments.PostSharp.Net45/HaystackExitAspect.cs
@@ -0,0 +1,34 @@
+﻿using Haystack.Bootstrap;
+using Haystack.Diagnostics.ObjectModel;
+using PostSharp.Aspects;
+using System;
+using System.Linq;
+
+namespace Haystack.Diagnostics.Amendments.PostSharp
+{
+    [Serializable]
+    public sealed class HaystackExitAspect : OnMethodBoundaryAspect
+    {
+        public HaystackExitAspect()
+        {
+            AspectPriority = int.MaxValue;
+        }
+
+        public override void OnExit(MethodExecutionArgs args)
+        {
+            HaystackBootstrapInitializer.InitializeIfNecessary();
+            AfterMethodCall(args);
+        }
+
+        private void AfterMethodCall(MethodExecutionArgs args)
+        {
+            MethodCallTraceProvider provider = MethodCallTraceContext.MethodCallTrace;
+            MethodCall methodCall = provider.ExitMethodCall();
+            methodCall.ReturnValue = provider.GetValue(args.ReturnValue);
+            foreach (int index in methodCall.Parameters.Where(param => param.Modifier != ParameterModifier.None).Select((value, index) => index))
+            {
+                methodCall.Parameters[index].OutputValue = provider.GetValue(args.Arguments[index]);
+            }
+        }
+    }
+}
