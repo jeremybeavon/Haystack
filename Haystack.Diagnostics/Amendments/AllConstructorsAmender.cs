e9f1dd9a76fe9968a9d94a5b9c9780aed2140bfa
jeremybeavon <jeremybeavon@users.noreply.github.com>
2016-05-30 14:15:36 +1200

    Add extra default amenders

---
+++ Haystack.Diagnostics/Amendments/AllConstructorsAmender.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+
+namespace Haystack.Diagnostics.Amendments
+{
+    public class AllConstructorsAmender : IConstructorAmender
+    {
+        public bool AmendConstructor(ConstructorInfo constructor)
+        {
+            return true;
+        }
+
+        public bool AmendConstructor(Type type, object[] parameters)
+        {
+            return true;
+        }
+    }
+}
