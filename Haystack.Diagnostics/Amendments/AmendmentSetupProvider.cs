897ee04410504c78252f24452216481a59f0f81f
jeremybeavon <jeremybeavon@users.noreply.github.com>
2016-05-30 15:42:45 +1200

    Improve support for strong naming with amendments

--- Haystack.Diagnostics/Amendments/AmendmentSetupProvider.cs
+++ Haystack.Diagnostics/Amendments/AmendmentSetupProvider.cs
@@ -16,31 +16,28 @@ namespace Haystack.Diagnostics.Amendments
     {
         public const string ConfigurationKey = "haystackConfiguration";
         public const string AfterthoughtAmenderExeFileName = "Afterthought.Amender.exe";
-        public const string AmendmentsDllFileName = "Haystack.Diagnostics.Amendments.dll";
+        public const string AmendmentsDllFileName = "Haystack.Diagnostics.Amendments.Afterthought.dll";
         
-        public AmendmentSetupProvider(string assemblyToAmend, IHaystackConfiguration configuration, string strongNameKey = null)
+        public AmendmentSetupProvider(string assemblyToAmend, IHaystackConfiguration configuration)
         {
             AssemblyToAmend = assemblyToAmend;
             Configuration = configuration;
-            StrongNameKey = strongNameKey;
             string currentLocation = Assembly.GetExecutingAssembly().AssemblyBaseDirectory();
-            AfterthoughtAmenderExe = Path.Combine(currentLocation, "Amendments", AfterthoughtAmenderExeFileName);
-            AmendmentsDll = Path.Combine(currentLocation, "Amendments", AmendmentsDllFileName);
+            AfterthoughtAmenderExe = Path.Combine(currentLocation, @"Amendments\Afterthought", AfterthoughtAmenderExeFileName);
+            AmendmentsDll = Path.Combine(currentLocation, @"Amendments\Afterthought", AmendmentsDllFileName);
         }
 
         public string AssemblyToAmend { get; set; }
 
         public IHaystackConfiguration Configuration { get; set; }
 
-        public string StrongNameKey { get; set; }
-
         public string AfterthoughtAmenderExe { get; set; }
 
         public string AmendmentsDll { get; set; }
 
-        public static void SetupIfNecessary(string assemblyToAmend, IHaystackConfiguration configuration, string strongNameKey = null)
+        public static void SetupIfNecessary(string assemblyToAmend, IHaystackConfiguration configuration)
         {
-            new AmendmentSetupProvider(assemblyToAmend, configuration, strongNameKey).SetupIfNecessary();
+            new AmendmentSetupProvider(assemblyToAmend, configuration).SetupIfNecessary();
         }
 
         public void SetupIfNecessary()
@@ -116,12 +113,12 @@ namespace Haystack.Diagnostics.Amendments
 
         private void ReSignAssemblyIfNecessary()
         {
-            if (StrongNameKey != null)
+            if (Configuration.Amendments.StrongNameKeyFile != null)
             {
                 IDictionary<string, string> properties = new Dictionary<string, string>
                 {
                     { "AssemblyPath", AssemblyToAmend },
-                    { "StrongNameKey", StrongNameKey }
+                    { "StrongNameKey", Configuration.Amendments.StrongNameKeyFile }
                 };
                 MsBuildRunner.RunMsBuildXml(Resources.AmendmentsStrongNameSetup, properties);
             }
