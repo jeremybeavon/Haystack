bf850480209e635cbbacfefc14406e83bb88c59f
jeremybeavon <jeremybeavon@users.noreply.github.com>
2016-05-31 16:59:30 +1200

    Update AppDomainExtensions.cs

--- Haystack.Core/AppDomainExtensions.cs
+++ Haystack.Core/AppDomainExtensions.cs
@@ -13,14 +13,27 @@ namespace Haystack.Core
     {
         public static void AddAssemblyResolveDirectory(this AppDomain appDomain, params string[] referencedDirectories)
         {
-            appDomain.AssemblyResolve += (sender, args) =>
+            appDomain.AssemblyResolve += new ReferencedDirectories(referencedDirectories).ResolveAssembly;
+        }
+
+        [Serializable]
+        private sealed class ReferencedDirectories
+        {
+            private string[] referencedDirectories;
+
+            public ReferencedDirectories(string[] referencedDirectories)
+            {
+                this.referencedDirectories = referencedDirectories;
+            }
+
+            public Assembly ResolveAssembly(object sender, ResolveEventArgs args)
             {
                 string assemblyFile = new AssemblyName(args.Name).Name + ".dll";
                 string assemblyPath = referencedDirectories
                     .Select(referencedDirectory => Path.Combine(referencedDirectory, assemblyFile))
                     .FirstOrDefault(file => File.Exists(file));
                 return assemblyPath == null ? null : Assembly.LoadFrom(assemblyPath);
-            };
+            }
         }
     }
 }
