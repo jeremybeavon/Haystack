fba1916e991add9ced1e11deeb0822d8d644dc74
jeremybeavon <jeremybeavon@users.noreply.github.com>
2016-06-08 14:35:08 +1200

    Add MethodCallStack

---
+++ Haystack.Diagnostics/MethodCallStack.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Haystack.Diagnostics.ObjectModel;
+
+namespace Haystack.Diagnostics
+{
+    internal sealed class MethodCallStack
+    {
+        public MethodCallStack()
+        {
+            CallStack = new Stack<MethodCall>(new[] { new MethodCall() });
+        }
+        
+        public Stack<MethodCall> CallStack { get; set; }
+
+        public bool IsInGetHashCode { get; set; }
+    }
+}
